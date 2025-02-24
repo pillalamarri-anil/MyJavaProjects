1b3ba88d54e300b07c2341aa3d8d2ed0e985dadf
PILLALAMARRI Anil <anil.pillalamarri@hexagon.com>
2025-02-24 20:48:59 +0530

    private protected test

---
+++ ExceptionHandling/ExceptionHandling/AccessModifer/Protected.java
@@ -0,0 +1,26 @@
+package AccessModifer;
+
+// protected methods are accessible within the same package even if it is not subclass
+// private methods are not accessible outside the class
+class test{
+
+    static protected void display(){
+        System.out.println("Protected method");
+    }
+
+    static private void display1(){
+        System.out.println("Private method");
+    }
+
+}
+
+public class Protected {
+
+    public static void main(String[] args) {
+        test.display();
+
+     //   test.display1();
+
+    }
+
+}
